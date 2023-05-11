468679fa9959a38c338d7361d4a074fefa140b53
josesoto <josesoto@itop.es>
2023-05-11 21:15:45 +0100

    feat(nav): added mor buttons

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -24,7 +24,7 @@ export const NavBar = () => {
         <></>
       )}
       <NavButton
-        iconName="schedule"
+        iconName="calendar-today"
         page="AppointmentFirst"
         iconColor={appTheme[theme].colorPrimary}
       />
