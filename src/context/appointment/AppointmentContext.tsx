f08b00def6dfbea44328ff328499c99bbbb96d13
josesoto <josesoto@itop.es>
2023-05-21 15:15:28 +0100

    feat(context): created context for the appointments

---
+++ src/context/appointment/AppointmentContext.tsx
@@ -0,0 +1,38 @@
+import { FC, createContext, useState } from 'react';
+
+export interface IAppointmentData {
+  barberId: number;
+  userId: number;
+  service: number[];
+  scheduledDateTime: string;
+}
+
+export interface IAppointment {
+  appointment: IAppointmentData;
+  setAppointment: Function;
+}
+
+export const AppointmentContext = createContext<IAppointment>({
+  appointment: {
+    barberId: 0,
+    userId: 0,
+    service: [] as number[],
+    scheduledDateTime: '',
+  },
+  setAppointment: () => {},
+});
+
+export const AppointmentProvider: FC<any> = ({ children }) => {
+  const [appointment, setAppointment] = useState<IAppointmentData>({
+    barberId: 0,
+    userId: 0,
+    service: [] as number[],
+    scheduledDateTime: '',
+  });
+
+  return (
+    <AppointmentContext.Provider value={{ appointment, setAppointment }}>
+      {children}
+    </AppointmentContext.Provider>
+  );
+};
